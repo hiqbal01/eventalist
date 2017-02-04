08a99fe10ed298d1e346f6ec31e520e200b45610
Haris Iqbal <harrisiqba@gmail.com>
2017-02-04 16:28:43 -0500

    first commit

--- js/new-age.js
+++ js/new-age.js
@@ -28,4 +28,18 @@
         }
     })
 
+    var $jittery = $('.jittery span'),
+      aText    = $jittery.text().split(''),
+      letters = '';
+  
+    for(var i = 0; i < aText.length; i++){
+        letters += '<span>'+aText[i]+'</span>';
+    }
+    
+    $jittery.empty().append(letters);
+    
+    $.each($('span', $jittery), function(i){
+        $(this).css('animation-delay', '-'+i+'70ms');
+    });
+
 })(jQuery); // End of use strict
